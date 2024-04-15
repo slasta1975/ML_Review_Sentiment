48137db84b61044afcd1958bd833c079476de2a6
Slawomir Stanowski <slasta1975@gmail.com>
2024-04-15 09:59:57 +0200

    Added loop for continuous review analysis until interrupted by use

--- ML_Review_Sentiment.py
+++ ML_Review_Sentiment.py
@@ -9,7 +9,7 @@ import sys
 
 POS_FILES_FEED = r"PP\M03\data\aclImdb\train\pos\*.txt"
 NEG_FILES_FEED = r"PP\M03\data\aclImdb\train\neg\*.txt"
-PUNCTUATIONS = ['.', ',', '?', '!', ':', ';', '-','"','<br />' ]
+PUNCTUATIONS = ['.', ',', '?', '!', ':', ';', '-', '"', '<br />']
 
 
 def preprocess_review(review):
@@ -23,38 +23,39 @@ def preprocess_review(review):
 
 def count_words(path_pattern):
     """
-    Calculates number of how many times a word has been used in defined 'files'. 
-    Returns dictionary with review and usage counter. 
+    Calculates number of how many times a word has been used in defined 'files'.
+    Returns dictionary with review and usage counter.
     """
     words_count = {}
     files = glob.glob(path_pattern)
     for file in files:
-        with open(file,encoding = 'utf-8') as stream:
+        with open(file, encoding='utf-8') as stream:
             content = stream.read()
         review = preprocess_review(content)
         for word in set(review):
-            words_count[word] = words_count.get(word,0) + 1
-    return words_count    
+            words_count[word] = words_count.get(word, 0) + 1
+    return words_count
+
 
 def compute_sentiment(review, pos_words_count, neg_words_count, detailing=False):
     # calculating sentiment of provided review
     cumulative_sentiment = 0
     for word in review:
-        pos_counter = pos_words_count.get(word,0)
-        neg_counter = neg_words_count.get(word,0)
-        total = pos_counter + neg_counter    
+        pos_counter = pos_words_count.get(word, 0)
+        neg_counter = neg_words_count.get(word, 0)
+        total = pos_counter + neg_counter
         if total == 0:
             word_sentiment = 0
         else:
-            word_sentiment = (pos_counter - neg_counter)/total
+            word_sentiment = (pos_counter - neg_counter) / total
         cumulative_sentiment += word_sentiment
         if detailing:
             print(word, word_sentiment)
-    return cumulative_sentiment/len(review)
+    return cumulative_sentiment / len(review)
 
 
 def print_sentiment(sentiment):
-            # Final statement
+    # Final statement
     if sentiment > 0:
         verdict = "positive"
     else:
@@ -64,25 +65,28 @@ def print_sentiment(sentiment):
 
 
 def main():
-
     pos_words_count = count_words(POS_FILES_FEED)
     neg_words_count = count_words(NEG_FILES_FEED)
 
+    while True:
         # getting review for analysis
-    review = input ("Provide your review: ")
-    if len(review) == 0:
-        print("No review for analysis")
-        sys.exit(1)
-    else:
-        preference = input("Interested in details? [y/n] ")
-        if preference.lower() == "y":
-            detailing = True
+        review = input("Provide your review: ")
+        if len(review) == 0:
+            print("No review for analysis")
+            sys.exit(1)
         else:
-            detailing = False
-        review = preprocess_review(review)
-        sentiment = compute_sentiment(review, pos_words_count, neg_words_count, detailing)
-        print_sentiment(sentiment)
+            preference = input("Interested in details? [y/n] ")
+            if preference.lower() == "y":
+                detailing = True
+            else:
+                detailing = False
+            review = preprocess_review(review)
+            sentiment = compute_sentiment(review, pos_words_count, neg_words_count, detailing)
+            print_sentiment(sentiment)
+            choice = input("Do you want to analyze another review? [y/n]: ")
+            if choice.lower() != "y":
+                break
 
-    
+
 if __name__ == "__main__":
     main()
